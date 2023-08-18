50846ac4503caee954fa9ffa96f591c3d77be6e2
Mikhail Stepanov <mstepanov@MacBook-Air-Mikhail.local>
2023-08-18 21:09:35 +0300

    prepared project

---
+++ ColorizingApp/Models/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  ColorizingApp
+//
+//  Created by Mikhail Stepanov on 8/18/23.
+//
+
+import Foundation
---
+++ ColorizingApp/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizingApp
+//
+//  Created by Mikhail Stepanov on 8/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
